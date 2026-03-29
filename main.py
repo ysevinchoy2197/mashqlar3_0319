10273b8824fe9ca4005d917a50856592ec86c0b0
Sevinch <ysevinchoy2197@gmail.com>
2026-03-29 16:38:23 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#1-misol
+s1 = lambda x: x if x > 0 else 0
+print(s1(-5))
+
+#2-misol
+s2 = lambda x: 'true' if x % 2 == 0 else 'False'
+print(s2(4))
+
+#3-misol
+s3 = lambda a, b: a if a > b else b
+print(s3(4, 5))
+
+#4-misol
+s4 = lambda x: 'Musbat' if x > 0 else 'Manfiy'
+print(s4(-1))
